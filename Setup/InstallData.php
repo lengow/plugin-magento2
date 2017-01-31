2a6acac8b1b6c606b91bf33afddb21d9d7ee9521
Alexandre Rabottin <alexandre.rabottin@lengow.com>
2017-01-31 10:37:33 +0100

    correction

--- Setup/InstallData.php
+++ Setup/InstallData.php
@@ -12,7 +12,7 @@
  *
  * @category    Lengow
  * @package     Lengow_Connector
- * @subpackage  Model
+ * @subpackage  Setup
  * @author      Team module <team-module@lengow.com>
  * @copyright   2017 Lengow SAS
  * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
