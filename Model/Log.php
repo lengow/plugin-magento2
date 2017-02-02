06432ed077647010508fb300184f97e95ffdf6cb
Alexandre Rabottin <alexandre.rabottin@lengow.com>
2017-02-02 16:36:36 +0100

    licence

--- Model/Log.php
+++ Model/Log.php
@@ -1,5 +1,21 @@
 <?php
-
+/**
+ * Copyright 2017 Lengow SAS
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @category    Lengow
+ * @package     Lengow_Connector
+ * @subpackage  Model
+ * @author      Team module <team-module@lengow.com>
+ * @copyright   2017 Lengow SAS
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
 
 namespace Lengow\Connector\Model;
 
@@ -9,7 +25,6 @@ use Magento\Framework\Model\Context;
 use Magento\Framework\Model\ResourceModel\Db\AbstractDb;
 use Magento\Framework\Registry;
 use Magento\Framework\Stdlib\DateTime\DateTime;
-
 
 class Log extends AbstractModel
 {
