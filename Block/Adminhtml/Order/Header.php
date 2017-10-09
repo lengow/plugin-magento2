275f15c459d4dab2c5b84fd27ffad6dd75d49cdd
Benjamin Le Nevé <benjamin.le-neve@lengow.com>
2017-10-09 10:57:55 +0200

    Add warning message in order page for preprod mode

---
+++ Block/Adminhtml/Order/Header.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Copyright 2017 Lengow SAS
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @category    Lengow
+ * @package     Lengow_Connector
+ * @subpackage  Block
+ * @author      Team module <team-module@lengow.com>
+ * @copyright   2017 Lengow SAS
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+namespace Lengow\Connector\Block\Adminhtml\Order;
+
+use Magento\Backend\Block\Template;
+use Lengow\Connector\Helper\Config as ConfigHelper;
+use Lengow\Connector\Helper\Data as DataHelper;
+use Lengow\Connector\Model\Export as Export;
+use Magento\Backend\Block\Template\Context;
+
+class Header extends Template
+{
+    /**
+     * @var \Lengow\Connector\Helper\Config Lengow config helper instance
+     */
+    protected $_configHelper;
+
+    /**
+     * Constructor
+     *
+     * @param \Magento\Backend\Block\Template\Context $context Magento block context instance
+     * @param array $data additional params
+     * @param \Lengow\Connector\Helper\Config $configHelper Lengow config helper instance
+     */
+    public function __construct(
+        Context $context,
+        array $data = [],
+        ConfigHelper $configHelper
+    )
+    {
+        parent::__construct($context, $data);
+        $this->_configHelper = $configHelper;
+    }
+
+    /**
+     * Preprod mode is enable
+     *
+     * @return boolean
+     */
+    public function preprodModeIsEnable()
+    {
+        return (bool)$this->_configHelper->get('preprod_mode_enable');
+    }
+}
