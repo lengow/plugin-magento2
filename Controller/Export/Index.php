88e75cca08e6ad14a16db1581cff350fd8fa9811
Benjamin Le Nevé <benjamin.le-neve@lengow.com>
2017-10-23 16:05:44 +0200

    Test - Delete exit(0) in export controller

--- Controller/Export/Index.php
+++ Controller/Export/Index.php
@@ -185,7 +185,6 @@ class Index extends Action
                     $this->getResponse()->setBody($this->_export->getTotalProduct());
                 } else {
                     $this->_export->exec();
-                    exit(0);
                 }
             } catch (\Exception $e) {
                 $errorMessage = '[Magento error] "' . $e->getMessage()
