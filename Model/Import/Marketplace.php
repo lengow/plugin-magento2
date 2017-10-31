305535c1d4c6fef1e4448a6780c8318345286550
Benjamin Le Nevé <benjamin.le-neve@lengow.com>
2017-10-31 16:12:25 +0100

    Hotfix: Add new verification in containOrderLine() function when action is not authorised for a marketplace

--- Model/Import/Marketplace.php
+++ Model/Import/Marketplace.php
@@ -289,15 +289,17 @@ class Marketplace extends AbstractModel
      */
     public function containOrderLine($action)
     {
-        $actions = $this->actions[$action];
-        if (isset($actions['args']) && is_array($actions['args'])) {
-            if (in_array('line', $actions['args'])) {
-                return true;
+        if (isset($this->actions[$action])) {
+            $actions = $this->actions[$action];
+            if (isset($actions['args']) && is_array($actions['args'])) {
+                if (in_array('line', $actions['args'])) {
+                    return true;
+                }
             }
-        }
-        if (isset($actions['optional_args']) && is_array($actions['optional_args'])) {
-            if (in_array('line', $actions['optional_args'])) {
-                return true;
+            if (isset($actions['optional_args']) && is_array($actions['optional_args'])) {
+                if (in_array('line', $actions['optional_args'])) {
+                    return true;
+                }
             }
         }
         return false;
