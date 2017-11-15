6fb85a2ea35ee8973605ca6c3175efef5bc00c9f
Alexandre Rabottin <alexandre.rabottin@lengow.com>
2017-11-15 11:46:27 +0100

    fix messages and style

--- view/adminhtml/web/js/order.js
+++ view/adminhtml/web/js/order.js
@@ -5,7 +5,8 @@ require(['jquery'], function ($) {
          * Show or not the product grid
          */
         $('.lengow-connector').on('click', '.lengow_import_orders-js', function () {
-            var href = $(this).attr('data-href');
+            var href = $(this).attr('data-href'),
+                lengowWrapperMessage = $('#lengow_wrapper_messages');
             $.ajax({
                 url: href,
                 method: 'POST',
@@ -20,6 +21,12 @@ require(['jquery'], function ($) {
                     $("#lengow_order_with_error").html(data.informations.order_with_error);
                     $("#lengow_order_to_be_sent").html(data.informations.order_to_be_sent);
                     $("#lengow_last_importation").html(data.informations.last_importation);
+                    var all_messages = '';
+                    $.each(data.informations.messages, function (index, message) {
+                        all_messages += message + '<br/>';
+                    });
+                    lengowWrapperMessage.html(all_messages);
+                    lengowWrapperMessage.show(0.25);
                     //reload the grid
                     var registry = require('uiRegistry');
                     registry.get('lengow_order_listing.lengow_order_listing').source.reload();
