e0188d6296a2c90c63a415d64ce0be1537ef09d7
Benjamin Le Nevé <benjamin.le-neve@lengow.com>
2020-01-22 17:27:30 +0100

    Delete preprod constants

--- Model/Connector.php
+++ Model/Connector.php
@@ -31,7 +31,10 @@ class Connector
     /**
      * @var string url of the API Lengow
      */
-    const LENGOW_API_URL = 'https://api.lengow.net';
+    // const LENGOW_API_URL = 'https://api.lengow.io';
+    // const LENGOW_API_URL = 'https://api.lengow.net';
+    const LENGOW_API_URL = 'http://api.lengow.rec';
+    // const LENGOW_API_URL = 'http://10.100.1.82:8081';
 
     /**
      * @var string url of access token API
--- view/adminhtml/web/js/home.js
+++ view/adminhtml/web/js/home.js
@@ -29,9 +29,15 @@ require(['jquery'], function( $ ) {
                 });
             };
             if (syncLink) {
-                syncIframe.src = '//cms.lengow.net/sync/';
+                // syncIframe.src = '//cms.lengow.io/sync/';
+                // syncIframe.src = '//cms.lengow.net/sync/';
+                syncIframe.src = '//cms.lengow.rec/sync/';
+                // syncIframe.src = '//cms.lengow.dev/sync/';
             } else {
-                syncIframe.src = '//cms.lengow.net/';
+                // syncIframe.src = '//cms.lengow.io/';
+                // syncIframe.src = '//cms.lengow.net/';
+                syncIframe.src = '//cms.lengow.rec/';
+                // syncIframe.src = '//cms.lengow.dev/';
             }
             syncIframe.src = syncIframe.src+'?lang='+isoCode+'&clientType=magento';
             $('#lengow-iframe').show();
