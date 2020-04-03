2ff135940c055ed68e4f942e1ea575b1b705b7a5
Benjamin Le Nevé <benjamin.le-neve@lengow.com>
2020-04-03 11:06:25 +0200

    MOD-128 - Feature: [import] Addition of order types in the order management screen

---
+++ Ui/Component/Listing/Column/OrderType.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * Copyright 2020 Lengow SAS
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @category    Lengow
+ * @package     Lengow_Connector
+ * @subpackage  UI
+ * @author      Team module <team-module@lengow.com>
+ * @copyright   2020 Lengow SAS
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+namespace Lengow\Connector\Ui\Component\Listing\Column;
+
+use Lengow\Connector\Model\Import\Order as LengowOrder;
+use Magento\Framework\View\Element\UiComponentFactory;
+use Magento\Framework\View\Element\UiComponent\ContextInterface;
+use Magento\Ui\Component\Listing\Columns\Column;
+
+class OrderTypes extends Column
+{
+    /**
+     * Constructor
+     *
+     * @param ContextInterface $context Magento ui context instance
+     * @param UiComponentFactory $uiComponentFactory Magento ui factory instance
+     * @param array $components component data
+     * @param array $data additional params
+     */
+    public function __construct(
+        ContextInterface $context,
+        UiComponentFactory $uiComponentFactory,
+        array $components = [],
+        array $data = []
+    ) {
+        parent::__construct($context, $uiComponentFactory, $components, $data);
+    }
+
+    /**
+     * Prepare Data Source
+     *
+     * @param array $dataSource row data source
+     *
+     * @return array
+     */
+    public function prepareDataSource(array $dataSource)
+    {
+        $dataSource = parent::prepareDataSource($dataSource);
+        if (isset($dataSource['data']['items'])) {
+            foreach ($dataSource['data']['items'] as &$item) {
+                if ($item['order_lengow_state'] !== null) {
+                    $status = $item['order_lengow_state'];
+                    switch ($status) {
+                        case LengowOrder::STATE_ACCEPTED:
+                            $translation = 'Accepted';
+                            break;
+                        case LengowOrder::STATE_WAITING_SHIPMENT:
+                            $translation = 'Awaiting shipment';
+                            break;
+                        case LengowOrder::STATE_SHIPPED:
+                            $translation = 'Shipped';
+                            break;
+                        case LengowOrder::STATE_REFUNDED:
+                            $translation = 'Refunded';
+                            break;
+                        case LengowOrder::STATE_CLOSED:
+                            $translation = 'Closed';
+                            break;
+                        case LengowOrder::STATE_CANCELED:
+                            $translation = 'Canceled';
+                            break;
+                        default:
+                            $translation = $status;
+                            break;
+                    }
+                    $item['order_lengow_state'] = '<span class="lgw-label lgw-label-' . $status . '">'
+                        . __($translation) . '</span>';
+                }
+            }
+        }
+        return $dataSource;
+    }
+}
