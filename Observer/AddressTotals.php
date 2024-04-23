e60ba6f237899d885e92e437103607ba4d0c299c
alexis.hermanns <alexis.hermanns@pcp-065>
2024-04-23 10:51:14 +0200

    PST-21179: [WIP] fixing rounding by plugins and observers

---
+++ Observer/AddressTotals.php
@@ -0,0 +1,120 @@
+<?php
+/**
+ * Copyright 2020 Lengow SAS
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @category    Lengow
+ * @package     Lengow_Connector
+ * @subpackage  Observer
+ * @author      Team module <team-module@lengow.com>
+ * @copyright   2020 Lengow SAS
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+namespace Lengow\Connector\Observer;
+
+use Lengow\Connector\Helper\Config as ConfigHelper;
+use Magento\Backend\Model\Session as BackendSession;
+use Magento\Framework\Event\Observer;
+use Magento\Framework\Event\ObserverInterface;
+
+/**
+ * AddressTotals
+ */
+class AddressTotals implements ObserverInterface
+{
+
+    /**
+     *
+     * @var ConfigHelper $configHelper
+     */
+    protected $configHelper;
+
+    /**
+     *
+     * @var BackendSession $backendSession
+     */
+    protected $backendSession;
+
+    /**
+     * AddressTotals constructor
+     */
+    public function __construct(
+        ConfigHelper $configHelper,
+        BackendSession $backendSession
+    ) {
+        $this->configHelper = $configHelper;
+        $this->backendSession = $backendSession;
+    }
+
+    /**
+     *
+     * Observer execution method
+     */
+    public function execute(Observer $observer)
+    {
+
+        if (!(bool)$this->backendSession->getIsFromlengow()) {
+            return;
+        }
+
+        $quote = $observer->getEvent()->getQuote();
+        $total = $observer->getEvent()->getTotal();
+        $lengowOrderData = $this->backendSession->getCurrentOrderLengowData();
+        $storeId =  $quote->getStore()->getId();
+
+        if (!(bool) $this->configHelper->get(ConfigHelper::CHECK_ROUNDING_ENABLED, $storeId)) {
+            return ;
+        }
+
+        if (!$lengowOrderData) {
+            return;
+        }
+
+        $totalLengow = (float) $lengowOrderData->total_order;
+        $taxLengow = (float) $lengowOrderData->total_tax;
+        $shippingLengow = (float) $lengowOrderData->shipping;
+        $subtotalLengow = $totalLengow - $taxLengow - $shippingLengow;
+        $subtotalInclTaxLengow = $totalLengow - $shippingLengow;
+
+        foreach ($total->getData() as $type => $amount) {
+            if ($amount === 0) {
+                continue;
+            }
+            if (($type === 'subtotal'
+                    || $type === 'base_subtotal'
+                    || $type === 'base_subtotal_with_discount'
+                    || $type === 'subtotal_with_discount')
+                    && $amount !== $subtotalLengow) {
+                $total->setData($type, $subtotalLengow);
+            }
+            if (($type === 'grand_total' || $type === 'base_grand_total')
+                    && $amount !== $totalLengow) {
+                $total->setData($type, $totalLengow);
+            }
+            if (($type === 'subtotal_incl_tax'
+                    || $type === 'base_subtotal_incl_tax'
+                    || $type === 'base_subtotal_total_incl_tax')
+                    && $amount !== $subtotalInclTaxLengow) {
+                $total->setData($type, $subtotalInclTaxLengow);
+            }
+            if (($type === 'tax_amount'
+                    || $type === 'base_tax_amount')
+                    && $amount !== $taxLengow) {
+                $total->setData($type, $taxLengow);
+            }
+            if (($type === 'shipping_incl_tax'
+                    || $type === 'base_shipping_incl_tax')
+                    && $amount !== $shippingLengow)
+            {
+                $total->setData($type, $shippingLengow);
+            }
+        }
+        $observer->getEvent()->setTotal($total);
+    }
+}
---
+++ Observer/QuoteItems.php
@@ -0,0 +1,112 @@
+<?php
+/**
+ * Copyright 2020 Lengow SAS
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @category    Lengow
+ * @package     Lengow_Connector
+ * @subpackage  Observer
+ * @author      Team module <team-module@lengow.com>
+ * @copyright   2020 Lengow SAS
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+namespace Lengow\Connector\Observer;
+
+use Magento\Backend\Model\Session as BackendSession;
+use Magento\Framework\Event\Observer;
+use Magento\Framework\Event\ObserverInterface;
+use Lengow\Connector\Helper\Config as ConfigHelper;
+
+/**
+ * QuoteItems
+ */
+class QuoteItems implements ObserverInterface
+{
+     /**
+     *
+     * @var ConfigHelper $configHelper
+     */
+    protected $configHelper;
+
+    /**
+     *
+     * @var BackendSession $backendSession
+     */
+    protected $backendSession;
+
+    /**
+     * QuoteItems constructor
+     */
+    public function __construct(
+        ConfigHelper $configHelper,
+        BackendSession $backendSession
+    ) {
+        $this->configHelper = $configHelper;
+        $this->backendSession = $backendSession;
+    }
+
+    /**
+     *
+     * @param Observer $observer
+     * @return type
+     */
+    public function execute(Observer $observer)
+    {
+
+        if (!(bool)$this->backendSession->getIsFromlengow()) {
+            return;
+        }
+
+        $quote = $observer->getEvent()->getQuote();
+        $products = $this->backendSession->getCurrentOrderLengowProducts();
+        $storeId = $quote->getStore()->getId();
+
+        if (!(bool) $this->configHelper->get(ConfigHelper::CHECK_ROUNDING_ENABLED, $storeId)) {
+            return ;
+        }
+
+        if (!$products) {
+            return;
+        }
+
+        foreach ($quote->getAllVisibleItems() as $item) {
+
+            if (!isset($products[$item->getProductId()])) {
+                continue;
+            }
+            
+            $product = $products[$item->getProductId()];
+
+            if (!$item->getTaxAmount() || !$product['tax_amount']) {
+                continue;
+            }
+
+            if (
+                $product['tax_amount'] === (float) $item->getTaxAmount()
+                && $product['amount'] === $item->getRowTotalInclTax()
+            ) {
+                continue;
+            }
+            $item->setTaxAmount($product['tax_amount']);
+            $item->setBaseTaxAmount($product['tax_amount']);
+            $item->setBaseRowTotal($product['amount'] - $product['tax_amount']);
+            $item->setRowTotal($product['amount'] - $product['tax_amount']);
+            $item->setRowTotalInclTax($product['amount']);
+            $item->setPrice($product['price_unit']);
+            $item->setPriceInclTax($product['amount']);
+            $item->setBasePriceInclTax($product['amount']);
+            $item->setCustomPrice($product['amount'] - $product['tax_amount']);
+            $item->setOriginalCustomPrice($product['amount'] - $product['tax_amount']);
+            $item->setBasePrice($product['amount'] - $product['tax_amount']);
+            $item->setOriginalPrice($product['amount'] - $product['tax_amount']);
+            $item->setBaseOriginalPrice($product['amount'] - $product['tax_amount']);
+            $item->setBaseRowTotalInclTax($product['amount']);
+        }
+    }
+}
