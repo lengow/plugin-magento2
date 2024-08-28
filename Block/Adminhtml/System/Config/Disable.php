ad3af6117d2b390bbe8f52bba038400494b72361
alexis.hermanns <alexis.hermanns@pcp-065>
2024-08-28 17:48:11 +0200

    misc(config): add developer mode configuration

--- Block/Adminhtml/System/Config/Disable.php
+++ Block/Adminhtml/System/Config/Disable.php
@@ -1,14 +1,56 @@
 <?php
+/**
+ * Copyright 2017 Lengow SAS
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @category    Lengow
+ * @package     Lengow_Connector
+ * @subpackage  Block
+ * @author      Team module <team-module@lengow.com>
+ * @copyright   2017 Lengow SAS
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
 namespace Lengow\Connector\Block\Adminhtml\System\Config;
 
 use Magento\Framework\Data\Form\Element\AbstractElement;
-use Magento\Config\Block\System\Config\Form\Field as ConfigField;
-
-class Disable extends ConfigField
+use Lengow\Connector\Helper\Config as LengowConfig;
+use Magento\Config\Block\System\Config\Form\Field as ConfigFormField;
+use Magento\Backend\Block\Template\Context;
+class Disable extends ConfigFormField
 {
+    /**
+     * @var LengowConfig
+     */
+    protected $lengowConfig;
+
+    /**
+     * Disable constructor
+     */
+    public function __construct(
+        Context $context,
+        LengowConfig $lengowConfig,
+        array $data = []
+    ) {
+        $this->lengowConfig = $lengowConfig;
+        parent::__construct($context, $data);
+    }
+
+    /**
+     * Retrieve element HTML markup
+     */
     protected function _getElementHtml(AbstractElement $element)
     {
+        if (!$this->lengowConfig->isDeveloperMode()) {
+            return $element->getElementHtml();
+        }
         $element->setDisabled('disabled');
+        $element->setComment(__('This field is disabled because you are in developer mode.'));
         return $element->getElementHtml();
     }
 }
--- Model/Config/Source/Environment.php
+++ Model/Config/Source/Environment.php
@@ -19,9 +19,21 @@
 namespace Lengow\Connector\Model\Config\Source;
 
 use Magento\Framework\Data\OptionSourceInterface;
+use Lengow\Connector\Helper\Config as LengowConfig;
 
 class Environment implements OptionSourceInterface
 {
+    /**
+     * @var LengowConfig Lengow config helper
+     */
+    protected $lengowConfig;
+
+    public function __construct(
+        LengowConfig $lengowConfig
+    )
+    {
+        $this->lengowConfig = $lengowConfig;
+    }
     /**
      * @const PRE_PROD_ENVIRONMENT
      */
@@ -34,10 +46,21 @@ class Environment implements OptionSourceInterface
     public const PROD_ENVIRONMENT = 'prod';
 
     /**
+     * @const DEVELOPER_MODE
+     */
+    public const DEVELOPER_MODE = 'developer';
+
+    /**
      * @return array[]
      */
     public function toOptionArray(): array
     {
+        if ($this->lengowConfig->isDeveloperMode()) {
+            return [
+                ['value' => static::DEVELOPER_MODE, 'label' => __('Test')],
+
+            ];
+        }
         return [
             ['value' => static::PRE_PROD_ENVIRONMENT, 'label' => __('Pre-Prod')],
             ['value' => static::PROD_ENVIRONMENT, 'label' => __('Production')]
